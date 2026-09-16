3063be422a3ededbf7a24a7acc0efe098f401ffa
Joao Saraiva <joao.saraiva@diconium.com>
2026-09-16 11:58:39 +0100

    Add default english profile

--- backend/src/main/java/spring/boot/udemy/course/backend/service/EnglishService.java
+++ backend/src/main/java/spring/boot/udemy/course/backend/service/EnglishService.java
@@ -1,11 +1,11 @@
 package spring.boot.udemy.course.backend.service;
+
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("EN")
+@Profile({ "EN", "default" })
 @Service("i18nService")
-public class EnglishService implements i18nService{
-
+public class EnglishService implements i18nService {
 
     @Override
     public String sayHello() {
